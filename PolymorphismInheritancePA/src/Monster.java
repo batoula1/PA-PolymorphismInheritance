d48dbc91c54a3250402ea131b2af6f66b62816fd
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-07-31 13:17:16 -0400

    LAB-Done

---
+++ PolymorphismInheritancePA/src/Monster.java
@@ -0,0 +1,15 @@
+public class Monster {
+    private String name;
+
+    public Monster(){
+        this.name = "Monster";
+    }
+
+    public Monster(String name){
+        this.name = name;
+    }
+
+    public String attack(){
+        return "!^_&^$@+%$* I don't know hwo to attack!";
+    }
+}
---
+++ PolymorphismInheritancePA/src/WaterMonster.java
@@ -0,0 +1,13 @@
+public class WaterMonster extends Monster{
+    public WaterMonster() {
+    }
+
+    public WaterMonster(String name) {
+        super(name);
+    }
+
+    @Override
+    public String attack() {
+        return "Attack with water!";
+    }
+}
